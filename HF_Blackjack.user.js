57fe27efb989c66a1cc99106c19edc5820547d0f
Adam <xadamxk@gmail.com>
2018-11-08 01:12:28 -0600

    Fixed history table sort bug

--- HF_Blackjack.user.js
+++ HF_Blackjack.user.js
@@ -289,10 +289,15 @@ function updateHistoryTable() {
             day: '2-digit', month: '2-digit', year: '2-digit',
             hour: '2-digit', minute: '2-digit'
         };
-        $.each(HFBJ.logs.reverse(), function (index, value) {
+        clearHistoryTable(false);
+        // http://www.javascripttutorial.net/javascript-array-sort/
+        var tempLog = HFBJ.logs.sort(function (x, y) {
+            return y.Date - x.Date;
+        });
+        $.each(HFBJ.logs, function (index, value) {
             $("#historyTabletbody").append($("<tr>").css(trCSS)
                 .append($("<td>").addClass("trow1").attr("colspan", "1")
-                    .append($("<strong>").text(value.Result).css("color",getResultColor(value.Result))))
+                    .append($("<strong>").text(value.Result).css("color", getResultColor(value.Result))))
                 .append($("<td>").addClass("trow1").attr("colspan", "1")
                     .append($("<strong>").text(new Date(value.Date).toLocaleTimeString([], dateFormat))))
                 .append($("<td>").addClass("trow1").attr("colspan", "1").attr(tdRightAlignAttribute)
@@ -301,6 +306,21 @@ function updateHistoryTable() {
                     .append($("<strong>").text(value.AmountWon).css("color", getAmountColor(value.AmountWon))))
             )
         });
+        HFBJ.logs = tempLog;
+    } else {
+        clearHistoryTable(true);
+    }
+}
+
+function clearHistoryTable(isFreshClear) {
+    $("#historyTabletbody").find("tr").each(function (index) {
+        if (index > 1) {
+            $(this).remove();
+        }
+    });
+    if (isFreshClear) {
+        $("#historyTabletbody").append($("<tr>").append($("<td>").addClass("trow1").attr("colspan", "4")
+            .append($("<strong>").text("No History Found"))));
     }
 }
 
@@ -314,6 +334,7 @@ function clearStats() {
     overallTotalBet = 0;
     overallTotalWon = 0;
     overallTotalNet = 0;
+    HFBJ.logs = [];
     updateStats(true);
 }
 
@@ -322,7 +343,7 @@ function updateStats(clearValues) {
         sessionNet = (sessionTotalWon - sessionTotalBet) + sessionTotalWon;
         overallTotalNet = (HFBJ.totalWon - HFBJ.totalBet) + HFBJ.totalWon;
     }
-    // Update table
+    // Update stats table
     $("#latestWinAmt").text(latestWinAmt);
     $("#sessionTotalGames").text(sessionTotalGames);
     $("#sessionTotalBet").text(sessionTotalBet);
@@ -332,6 +353,7 @@ function updateStats(clearValues) {
     $("#overallTotalBet").text(overallTotalBet);
     $("#overallTotalWon").text(overallTotalWon).css("color", getAmountColor(overallTotalWon));
     $("#overallTotalNet").text(overallTotalNet).css("color", getAmountColor(overallTotalNet));
+    updateHistoryTable();
 }
 
 function getAmountColor(amount) {
@@ -585,6 +607,5 @@ function initializeLogFromMemory() {
         }
     } else {
         HFBJ = JSON.parse(HFBJ);
-        console.log(HFBJ);
     }
 }
